857babf3d4ab62b32d00c54eabbe3bcf5a3f24ff
Alessandro Surace <zioalex@gmail.com>
2022-05-28 19:33:35 +0200

    Week 2 without dump

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -65,10 +65,10 @@ def run(data_path, log_top):
 
     # select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(experiment_ids=experiment.experiment_id, max_results=1, order_by=["metrics.rmse ASC"])[0]
 
     # register the best model
-    # mlflow.register_model( ... )
+    mlflow.register_model(model_uri="runs:/" + best_run.info.run_id + "/model", name="Best_RMSE")
 
 
 if __name__ == '__main__':
--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -5,6 +5,11 @@ import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+import mlflow
+
+mlflow.set_tracking_uri("sqlite:///../mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment")
+mlflow.sklearn.autolog()
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -12,15 +17,15 @@ def load_pickle(filename: str):
 
 
 def run(data_path):
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
-
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
